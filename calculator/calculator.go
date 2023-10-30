ec2c044782e225c72d7b64c991228d9a9ed8d10b
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-10-30 19:38:46 +0100

    feat(calculator): Add Substract

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -3,3 +3,7 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+func Substract(a, b float64) float64 {
+	return a - b
+}
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -13,3 +13,12 @@ func TestAdd(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+
+func TestSubstract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Substract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
