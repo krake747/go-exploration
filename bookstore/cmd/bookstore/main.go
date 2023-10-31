60b6c0e845cc90b6f27236f60a01071cf25ef89b
Kevin K. <41166160+krake747@users.noreply.github.com>
2023-10-31 10:52:09 +0000

    feat(bookstore): Add variables

--- bookstore/cmd/bookstore/main.go
+++ bookstore/cmd/bookstore/main.go
@@ -4,9 +4,26 @@ import "fmt"
 
 func main() {
 	var title string
-	var copies int
 	title = "For the Love of Go"
+	var author string
+	author = "John Arundel"
+	var copies int
 	copies = 99
-	fmt.Println(title)
+	var inStock bool
+	inStock = true
+	var royaltyPercentage float64
+	royaltyPercentage = 12.5
+	var isSpecialOffer bool
+	isSpecialOffer = true
+
+	printTitle(title)
+	fmt.Println(author)
 	fmt.Println(copies)
+	fmt.Println("In Stock: ", inStock)
+	fmt.Println(royaltyPercentage)
+	fmt.Println("Special Offer: ", isSpecialOffer)
+}
+
+func printTitle(title string) {
+	fmt.Println("Title: ", title)
 }
