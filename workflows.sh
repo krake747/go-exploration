9214480f0b6494376dd8560592192642d234f513
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-10-31 23:06:05 +0100

    feat(go): Create mono repo and github actions

---
+++ workflows.sh
@@ -0,0 +1,15 @@
+# read the workflow template
+WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yml)
+
+mkdir -p .github/workflows
+
+# iterate each go package in the src directory
+for package in $(ls src); do
+	echo "generating workflow for src/${package}"
+
+	# replace template package placeholder with package name
+	WORKFLOW=$(echo "${WORKFLOW_TEMPLATE}" | sed "s/{{package}}/${package}/g")
+	
+	# save workflow to .github/workflows/{package}
+	echo "${WORKFLOW}" > .github/workflows/${package}.yaml
+done
